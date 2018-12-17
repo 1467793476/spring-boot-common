cb9d3b47888cada1f1b048d27ff8f3ef76e621b7
wwzzqx <1467793476@qq.com>
2018-12-17 13:08:08 +0800

    "this is update"

--- src/main/java/com/baizhi/SpringBootCommonApplication.java
+++ src/main/java/com/baizhi/SpringBootCommonApplication.java
@@ -8,21 +8,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class SpringBootCommonApplication {
 
     public static void main(String[] args) {
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
         SpringApplication.run(SpringBootCommonApplication.class, args);
     }
 
